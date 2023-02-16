e2f990592b4d4fe05533bc529f3a090a33db586a
manayagwan <paulinamondayyagwan@gmail.com>
2023-02-16 10:41:26 -0800

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - entry point
  *
- * return: always 0 (success)
+ * Description: 'uses puts to print'
+ *
+ * Return: 0 (success)
  */
 int main(void)
-
 {
-
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
