025bb4ebc16846a14392683b6114ccfff4267e63
manayagwan <paulinamondayyagwan@gmail.com>
2023-02-22 02:48:28 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	int l;
+
+	for (l = 'a' ; l <= 'z' ; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+}
