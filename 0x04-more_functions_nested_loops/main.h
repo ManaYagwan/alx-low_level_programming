7b7ff33c56900e67ade5f26c82e02e52e61f404f
manayagwan <paulinamondayyagwan@gmail.com>
2023-02-24 04:11:30 -0800

    work

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
+int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -10,3 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
+#endif /* MAIN_H */
