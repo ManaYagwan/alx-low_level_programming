04b9904554e90ee433bdb607dc02c9d5f10b0281
manayagwan <paulinamondayyagwan@gmail.com>
2023-02-27 06:20:34 -0800

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset the value of the giving int to 98
+ *@n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
