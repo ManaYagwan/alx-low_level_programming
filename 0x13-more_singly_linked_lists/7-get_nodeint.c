dd81801e2e2a5ce584f35b9918b0dc7ba33756e0
manayagwan <paulinamondayyagwan@gmail.com>
2023-05-02 07:21:51 -0700

    test

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include"lists.h"
+/**
+ * get_nodeint_at_index - function name function that
+ * @head :pointer
+ * @index : int
+ * Return : null
+ */
+
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int alx;
+
+	alx = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL)
+	{
+		if (alx == index)
+		alx++;
+			head = head->next;
+	}
+	return (head);
+}
